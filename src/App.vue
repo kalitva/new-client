a03712c4db290eb9cec35c555f7f892cfdb27273
Stanislav Semenchuk <undefy23@gmail.com>
2022-08-17 16:28:54 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <hello-world msg="HELLOWORLD" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
